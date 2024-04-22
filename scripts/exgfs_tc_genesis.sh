41cf90f5efa11a4f2836a387205806080a54a568
Innocent Souopgui <Innocent.Souopgui@noaa.gov>
2024-04-22 21:22:23 +0000

    Fix the detection of Jet in tc_genesis and tc_track

--- scripts/exgfs_tc_genesis.sh
+++ scripts/exgfs_tc_genesis.sh
@@ -60,7 +60,7 @@ elif [[ -d /work ]] ; then
   machine=orion
   ${USHens_tracker}/extrkr_tcv_gfs.sh ${loopnum} ${cmodel} ${pert} ${pertdir} #2>&1 >${outfile}
 
-elif [[ -d /lfs3 ]] ; then
+elif [[ -d /lfs1 ]] ; then
   # We are on NOAA Jet
   machine=jet
   ${USHens_tracker}/extrkr_tcv_gfs.sh ${loopnum} ${cmodel} ${pert} ${pertdir} #2>&1 >${outfile}
--- scripts/exgfs_tc_track.sh
+++ scripts/exgfs_tc_track.sh
@@ -44,7 +44,7 @@ elif [[ -d /work ]] ; then
   machine=orion
   ${USHens_tracker}/extrkr_gfs.sh ${loopnum} ${cmodel} ${ymdh} ${pert} ${pertdir} #2>&1 >${outfile}
 
-elif [[ -d /lfs3 ]] ; then
+elif [[ -d /lfs1 ]] ; then
   # We are on NOAA Jet
   machine=jet
   ${USHens_tracker}/extrkr_gfs.sh ${loopnum} ${cmodel} ${ymdh} ${pert} ${pertdir} #2>&1 >${outfile}
